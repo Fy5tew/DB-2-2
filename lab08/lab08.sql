34f82342f50418d81e4ebc4a83415d0259e1c7be
Nikita <nikturchinovich@mail.ru>
2024-03-21 16:48:59 +0300

    lab08 is done

---
+++ lab08/lab08.sql
@@ -0,0 +1,26 @@
+-- Задание №1
+-- %ORACLE_HOME%\NETWORK\ADMIN\SQLNET.ORA
+-- D:\Apps\Oracle\OracleDB\OracleDB19cHome\network\admin\sqlnet.ora
+-- %ORACLE_HOME%\NETWORK\ADMIN\TNSNAMES.ORA
+-- D:\Apps\Oracle\OracleDB\OracleDB19cHome\network\admin\tnsnames.ora
+
+
+-- Задание №2
+-- Test_Basic
+
+
+-- Задание №3
+-- sqlplus /nolog
+-- connect system/OracleDB19c@localhost:1521/oracledb;
+
+
+-- Задание №4
+-- Oracle Net Manager
+
+
+-- Задание №5
+-- Test_TNS
+
+
+-- sqlplus /nolog
+-- connect system/OracleDB19c@test;
